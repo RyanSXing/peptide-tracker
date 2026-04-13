473414e4eef9669444018390bde993e2afa849c8
Ryan Xing <ryanxing06@gmail.com>
2026-04-12 23:06:30 -0400

    Initial Commit

---
+++ peptide tracker/peptide_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  peptide_trackerApp.swift
+//  peptide tracker
+//
+//  Created by Ryan Xing on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct peptide_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

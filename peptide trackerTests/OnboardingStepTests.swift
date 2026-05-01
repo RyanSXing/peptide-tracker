be50e7aefc92693646e6b2a720397dac053f18b8
Ryan Xing <ryanxing06@gmail.com>
2026-04-30 22:37:26 -0400

    Add unit tests for OnboardingStep enum
    - Test Equatable conformance (login == login)
    - Test enum case accessibility
    - Test future extensibility verification
    
    Follows established testing patterns from TimeRangeTests.swift

---
+++ peptide trackerTests/OnboardingStepTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import peptide_tracker
+
+final class OnboardingStepTests: XCTestCase {
+
+    func testEquatableConformance() {
+        let step1 = OnboardingStep.login
+        let step2 = OnboardingStep.login
+
+        XCTAssertEqual(step1, step2)
+    }
+
+    func testEnumCaseAccessibility() {
+        let loginStep = OnboardingStep.login
+
+        XCTAssertNotNil(loginStep)
+    }
+
+    func testFutureExtensibility() {
+        let currentStep = OnboardingStep.login
+
+        switch currentStep {
+        case .login:
+            break
+        }
+    }
+}
